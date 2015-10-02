291d46a7952e9ec1ca208fc9a58df8611e52b5fb
Ben <BforBen@users.noreply.github.com>
2015-10-02 21:30:33 +0100

    Initial commit

---
+++ Needs/Needs.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace Needs.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var settings = GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
---
+++ Needs/Needs.Api/Controllers/EsdController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Needs.Api.Models;
+
+namespace Needs.Api.Controllers
+{
+    [RoutePrefix("{type}")]
+    public class EsdController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public IHttpActionResult List(string Type)
+        {
+            return Ok(DocumentDBRepository<EsdEntry>.GetItems(e => e.Type == Type));
+        }
+    }
+}
