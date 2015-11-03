cb059e98d3eb892347a4ce70de90bcad8c2a07a0
Ben <BforBen@users.noreply.github.com>
2015-11-03 10:47:07 +0000

    Initial test with MongoDB

--- Needs/Needs.Api/Controllers/EsdController.cs
+++ Needs/Needs.Api/Controllers/EsdController.cs
@@ -6,16 +6,20 @@ using System.Net.Http;
 using System.Web.Http;
 using Needs.Api.Models;
 
+using MongoDB.Driver;
+
 namespace Needs.Api.Controllers
 {
     [RoutePrefix("{type}")]
     public class EsdController : ApiController
     {
+        IMongoDatabase mdb = new MongoClient("mongodb://api:Guildford1#@ds034198.mongolab.com:34198").GetDatabase("gbc-needs");
+
         [Route]
         [HttpGet]
-        public IHttpActionResult List(string Type)
+        public IHttpActionResult List(string type)
         {
-            return Ok(DocumentDBRepository<EsdEntry>.GetItems(e => e.Type == Type));
+            return Ok(mdb.GetCollection<EsdEntry>(type));
         }
     }
 }
