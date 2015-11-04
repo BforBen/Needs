aefb32cea518859f4670ffcbf5c0974d4fc5802e
Ben <BforBen@users.noreply.github.com>
2015-11-04 12:05:11 +0000

    Add filter

--- Needs/Needs.Api/Controllers/EsdController.cs
+++ Needs/Needs.Api/Controllers/EsdController.cs
@@ -9,21 +9,29 @@ using System.Web.Http.Description;
 using Needs.Api.Models;
 
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Needs.Api.Controllers
 {
-    //[RoutePrefix("{type}")]
+    [RoutePrefix("{type:values(_test|benefits|channels|duties|functions|interactions|life-events|needs|powers|processes|services|subjects)}")]
     public class EsdController : ApiController
     {
         IMongoDatabase mdb = new MongoClient("mongodb://api:Guildford1@ds034198.mongolab.com:34198/gbc-needs").GetDatabase("gbc-needs");
         
-        [Route("{type:values(needs|services|functions)}", Order = 5)]
+        [Route("{id:int?}")]
         [HttpGet]
         [ResponseType(typeof(IList<EsdEntry>))]
-        public async Task<IHttpActionResult> List(string type)
+        public async Task<IHttpActionResult> List(string type, int? id = null)
         {
-            var data = await mdb.GetCollection<EsdEntry>(type).AsQueryable().ToListAsync();
-            return Ok(data);
+            FilterDefinition<EsdEntry> filter = null;
+
+            if (id.HasValue)
+            {
+                filter = Builders<EsdEntry>.Filter.Eq<int>("id", id.Value);
+            }
+
+            var data = await mdb.GetCollection<EsdEntry>(type).FindAsync(filter);
+            return Ok(await data.ToListAsync());
         }
     }
 }
